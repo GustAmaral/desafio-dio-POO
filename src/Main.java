59d99659b9191c73f5f642ef5253b174d986e2d3
Gustavo <gustamaral11@gmail.com>
2025-07-31 18:02:43 -0300

    Conclusão do desafio.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -20,8 +19,43 @@ public class Main {
         mentoria.setDescricao("descrição mentoria de java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp java developer");
+        bootcamp.setDescricao("Descrição Bootcamp java developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devGustavo = new Dev();
+        devGustavo.setNome("Gustavo");
+        devGustavo.inscreverBootcamp(bootcamp);
+        System.out.printf("Conteúdos inscritos Gustavo: %s\n", devGustavo.getConteudosInscritos());
+        devGustavo.progredir();
+        devGustavo.progredir();
+        System.out.println("-");
+        System.out.printf("Conteúdos inscritos Gustavo: %s\n", devGustavo.getConteudosInscritos());
+        System.out.printf("Conteúdos concluídos Gustavo: %s\n", devGustavo.getConteudosConcluidos());
+        System.out.printf("XP: %.1f\n", devGustavo.calcularTotalXp());
+
+        System.out.println("----------------------------------------------------------------------------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.printf("Conteúdos inscritos João: %s\n", devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("-");
+        System.out.printf("Conteúdos inscritos João: %s\n", devJoao.getConteudosInscritos());
+        System.out.printf("Conteúdos concluídos João: %s\n", devJoao.getConteudosConcluidos());
+        System.out.printf("XP: %.1f\n", devJoao.calcularTotalXp());
+
+
+
     }
 }
